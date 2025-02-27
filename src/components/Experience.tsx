cb75c3c7c8f2776c4941c35fbac19845c59bf1df
Hemit Patel <hemitvpatel@gmail.com>
2025-02-26 20:09:18 -0500

    added bg

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -5,13 +5,13 @@ import { useThree } from "@react-three/fiber";
 import { useState, useEffect } from "react";
 import * as THREE from "three";
 import Sun from "./models/Sun";
-import { Cloud } from "./models/Cloud";
 import { CharacterController } from "./CharacterController";
 import { Stage } from "./Stage";
+import { Background } from "./models/Background";
 
 export const Experience = () => {
   const { scene } = useThree();
-  scene.background = new THREE.Color("#87CEEB");
+  scene.background = new THREE.Color("#e3daf7");
 
   return (
     <>
@@ -22,25 +22,12 @@ export const Experience = () => {
       {/* BACKGROUND GROUND */}
       <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[50, 50]} />
-        <meshBasicMaterial color="#87CEEB" toneMapped={false} />
+        <meshBasicMaterial color="#e3daf7" toneMapped={false} />
       </mesh>
 
       <Sun />
 
-      {/* CLOUDS - Updated to include the new position */}
-      {[
-        [-15, -6, -50],
-        [-10, -4, -50],
-        [4, -4, -50],
-        [10, -6, -55],
-        [-5, -5, -60],
-        [6, -5, -65],
-        [15, -10, -65],
-
-
-      ].map((pos, index) => (
-        <Cloud key={index} scale={[1, 1, 1]} position={pos} />
-      ))}
+      <Background position={[0,-1.5,-10]}/>
 
       {/* INTERACTIVE ELEMENTS */}
       <Torii scale={[16, 16, 16]} position={[0, 2, -22]} rotation-y={1.25 * Math.PI} />
@@ -49,7 +36,7 @@ export const Experience = () => {
 
       {/* STAGE */}
       <RigidBody colliders={false} type="fixed" position-y={-0.5} friction={2}>
-        <Stage scale={[0.8, 0.8, 0.8]} position={[0.5, 0, 0]} />
+        <Stage scale={[0.9, 0.9, 0.9]} position={[0.5, 0, 0]} />
         
         {/* CharacterController stays here and will not move */}
         <RigidBody type="fixed" position={[0, 1, 0]}>
