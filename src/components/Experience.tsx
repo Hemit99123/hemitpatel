15423fa1910023b1c5cc50b0913b0b3913c1a638
Hemit Patel <hemitvpatel@gmail.com>
2025-02-26 19:53:07 -0500

    added clouds

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,10 +1,4 @@
-import {
-  Cylinder,
-  Environment,
-  OrbitControls,
-  useCursor,
-  Html,
-} from "@react-three/drei";
+import { Cylinder, Environment, OrbitControls, useCursor, Html, } from "@react-three/drei";
 import { CylinderCollider, RigidBody } from "@react-three/rapier";
 import { Torii } from "./models/Torii";
 import { useThree } from "@react-three/fiber";
@@ -17,20 +11,14 @@ import { Stage } from "./Stage";
 
 export const Experience = () => {
   const { scene } = useThree();
-  
   scene.background = new THREE.Color("#87CEEB");
 
   return (
     <>
-    <OrbitControls />
+      <OrbitControls />
       <Environment preset="sunset" />
       <ambientLight intensity={1} />
-      <directionalLight
-        position={[5, 5, 5]}
-        intensity={0.3}
-        castShadow
-        color={"#9e69da"}
-      />
+      <directionalLight position={[5, 5, 5]} intensity={0.3} castShadow color={"#9e69da"} />
 
       {/* BACKGROUND GROUND */}
       <mesh position={[0, -1.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
@@ -38,12 +26,20 @@ export const Experience = () => {
         <meshBasicMaterial color="#87CEEB" toneMapped={false} />
       </mesh>
 
-
-      
       <Sun />
 
-      {/* CLOUDS */}
-      {[[-15, 2, -30], [10, 4, -30], [-5, 3, -30], [5, 11, -30], [12, 3, -30]].map((pos, index) => (
+      {/* CLOUDS - Updated to include the new position */}
+      {[
+        [-15, -6, -50],
+        [-10, -4, -50],
+        [4, -4, -50],
+        [10, -6, -55],
+        [-5, -5, -60],
+        [6, -5, -65],
+        [15, -10, -65],
+
+
+      ].map((pos, index) => (
         <Cloud key={index} scale={[1, 1, 1]} position={pos} />
       ))}
 
@@ -52,22 +48,14 @@ export const Experience = () => {
       <Torii scale={[10, 10, 10]} position={[-8, 0.8, -20]} rotation-y={1.4 * Math.PI} />
       <Torii scale={[10, 10, 10]} position={[8, 0.8, -20]} rotation-y={Math.PI} />
 
-      {/* Removed group with rotation-y to prevent unintended rotation */}
-      
       {/* STAGE */}
-      <RigidBody
-        colliders={false}
-        type="fixed"
-        position-y={-0.5}
-        friction={2}
-      >
-        <Stage scale={[0.8,0.8,0.8]} position={[0.5,0,0]}/>
-
+      <RigidBody colliders={false} type="fixed" position-y={-0.5} friction={2}>
+        <Stage scale={[0.8, 0.8, 0.8]} position={[0.5, 0, 0]} />
+        
         {/* CharacterController stays here and will not move */}
         <RigidBody type="fixed" position={[0, 1, 0]}>
           <CharacterController />
         </RigidBody>
-
       </RigidBody>
     </>
   );
