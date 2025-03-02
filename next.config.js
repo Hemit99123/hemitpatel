463d8f0efab02ca1f6f4bdbe6961db3a787b62bb
Hemit Patel <hemitvpatel@gmail.com>
2025-03-02 15:26:21 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 const nextConfig = {
-  /* config options here */
-  reactStrictMode,
-  ignoreDuringBuilds,
+  reactStrictMode: false, // Set to true to enable React's strict mode
 };
 
 export default nextConfig;
