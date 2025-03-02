674e8058f6f47361095386117fa9a6b2472415bb
Hemit Patel <hemitvpatel@gmail.com>
2025-03-02 17:00:59 -0500

    removed useeffect logic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,9 @@
 "use client"
 
 import MainScene from "../components/MainScene";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 1000);
-  })
-  
   return (
     <>
       <MainScene />
